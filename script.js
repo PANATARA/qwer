40a144002e61d32bbd9e18896a4604c1425b52fc
Andrew <125561357+PANATARA@users.noreply.github.com>
2023-06-16 19:03:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,88 @@
-const state = {};
-const carouselList = document.querySelector('.carousel__list');
-const carouselItems = document.querySelectorAll('.carousel__item');
-const elems = Array.from(carouselItems);
+var positionY = 0;
 
-carouselList.addEventListener('click', function (event) {
-  var newActive = event.target;
-  var isItem = newActive.closest('.carousel__item');
+const sectionItems = document.getElementsByClassName("section_item");
 
-  if (!isItem || newActive.classList.contains('carousel__item_active')) {
-    return;
-  };
-  
-  update(newActive);
-});
+// Создаем пустой массив для хранения элементов
+const itemsArray = [];
 
-const update = function(newActive) {
-  const newActivePos = newActive.dataset.pos;
+// Итерируемся по всем элементам и добавляем их в массив
+for (let i = 0; i < sectionItems.length; i++) {
+    itemsArray.push(sectionItems[i]);
+}
+const currentScale = [];
+var currentMarginTop = parseInt(window.getComputedStyle(itemsArray[1]).zIndex);
 
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
-  
-  current.classList.remove('carousel__item_active');
-  
-  [current, prev, next, first, last].forEach(item => {
-    var itemPos = item.dataset.pos;
+for (let i = 0; i < itemsArray.length; i++) {
+    currentScale.push(parseFloat(window.getComputedStyle(itemsArray[i]).scale));
+    
+}
+var abc = -230;
+function upItem(){
+    positionY++;
+    abc = abc + 10;
+    console.log(abc);
+    // if (positionY == 1) {
+        sectionItems[positionY].style.zIndex = "7";
+        sectionItems[positionY].style.marginTop = abc + "px";
+        sectionItems[positionY - 1].style.zIndex = "6";
+        sectionItems[positionY - 1].style.marginBottom = "240px";
+        setInterval(function() {
+            sectionItems[positionY - 1].style.marginBottom = "80px";
+          }, 400)
+    // }
+    // if (positionY == 2) {
+    //     document.getElementById('Nature').style.zIndex = "7";
+    //     document.getElementById('Nature').style.marginTop = abc + "px";
+    //     document.getElementById('Nature').style.scale = "1";
+    //     document.getElementById('People').style.zIndex = "6";
+    //     document.getElementById('People').style.marginBottom = "240px";
+    //     setInterval(function() {
+    //         document.getElementById('People').style.marginBottom = "80px";
+    //       }, 400)
+    // }
+    // if (positionY == 3) {
+    //     document.getElementById('inventions').style.zIndex = "7";
+    //     document.getElementById('inventions').style.marginTop = abc + "px";
+    //     document.getElementById('inventions').style.scale = "1";
+    //     document.getElementById('Nature').style.zIndex = "6";
+    //     document.getElementById('Nature').style.marginBottom = "240px";
+    //     setInterval(function() {
+    //         document.getElementById('Nature').style.marginBottom = "80px";
+    //       }, 400)
+    // }
+    // if (positionY == 4) {
+    //     document.getElementById('Facts').style.zIndex = "7";
+    //     document.getElementById('Facts').style.marginTop = abc + "px";
+    //     document.getElementById('Facts').style.scale = "1";
+    //     document.getElementById('inventions').style.zIndex = "6";
+    //     document.getElementById('inventions').style.marginBottom = "240px";
+    //     setInterval(function() {
+    //         document.getElementById('inventions').style.marginBottom = "80px";
+    //       }, 400)
+    // }
+    // if (positionY == 5) {
+    //     document.getElementById('literature').style.zIndex = "7";
+    //     document.getElementById('literature').style.marginTop = abc + "px";
+    //     document.getElementById('literature').style.scale = "1";
+    //     document.getElementById('Facts').style.zIndex = "6";
+    //     document.getElementById('Facts').style.marginBottom = "240px";
+    //     setInterval(function() {
+    //         document.getElementById('Facts').style.marginBottom = "80px";
+    //       }, 400)
+    // }
+    // if (positionY == 6) {
+    //     document.getElementById('History').style.zIndex = "7";
+    //     document.getElementById('History').style.marginTop = abc + "px";
+    //     document.getElementById('History').style.scale = "1";
+    //     document.getElementById('literature').style.zIndex = "6";
+    //     document.getElementById('literature').style.marginBottom = "240px";
+    //     setInterval(function() {
+    //         document.getElementById('literature').style.marginBottom = "80px";
+    //       }, 400)
+    // }
+}
 
-    item.dataset.pos = getPos(itemPos, newActivePos)
-  });
-};
-
-const getPos = function (current, active) {
-  const diff = current - active;
-
-  if (Math.abs(current - active) > 2) {
-    return -current
-  }
-
-  return diff;
+function downItem(){
+    positionY--;
+    
 }
